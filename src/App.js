d323cbbb6a354867a508d86435aae51acd2fc777
imsamet <imsametcetin@gmail.com>
2022-01-05 17:28:29 +0300

    add index

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Index from './components/layout/index';
 
 function App() {
   return (
-    <div>
-    </div>
+    <>
+      <Index/>
+    </>
   );
 }
 
---
+++ src/components/layout/index/index.js
@@ -0,0 +1,25 @@
+import Style from './style.module.css'
+
+import { Container, Row, Col } from 'react-bootstrap'
+
+export default function Index () {
+
+    return(
+        <>
+            <Container>
+
+                <Row>
+                    <Col xs={12} sm={12} md={12} lg={4} xl={4} xxl={4}>
+                    xs=12 md=8
+                    </Col>
+                    <Col xs={12} sm={12} md={6} lg={4} xl={4} xxl={4}>
+                    xs=6 md=4
+                    </Col>
+                    <Col xs={12} sm={12} md={6} lg={4} xl={4} xxl={4}>
+                    xs=6 md=4
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    )
+}
